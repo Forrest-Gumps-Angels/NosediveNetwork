fa0d70f537dab9121c0764c142f466785feff1ce
Morten Lenschow Hansen <au616040@uni.au.dk>
2020-05-05 11:19:07 +0200

    things

---
+++ NosediveNetwork/Models/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace NosediveNetwork.Models
+{
+    public class Circle
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CircleId { get; set;  }
+        public string UserId { get; set; }
+        public string PostId { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format($"Circle ID {CircleId}, UserId {UserId}, PostId {PostId}");
+        }
+    }
+}
---
+++ NosediveNetwork/Services/Picture.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//Help from: https://www.codeproject.com/articles/708140/uploading-and-viewing-images-with-asp-net-mvc-and
+
+
+namespace NosediveNetwork.Services
+{
+    public class Picture
+    {
+        public ObjectId Id { get; set; }
+        public string FileName { get; set; }
+        public string PictureDataAsString { get; set; }
+    }
+}
